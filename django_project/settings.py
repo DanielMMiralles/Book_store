9de3232d2847c277fd3480b15044acca4eda8253
Daniel <damm172004@gmail.com>
2024-08-24 16:53:02 -0400

    Models added

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #local
-    'accounts.apps.Config',
+    'accounts.apps.AccountsConfig',
     'books.apps.BooksConfig',
 ]
 
